3a5713fb2142b9b45d78d8b9a7d76cc58692b5fb
Ferdinand Putra Gumilang Silalahi <gumilangsilalahi3@gmail.con>
2023-03-27 09:46:59 +0700

    added contact view

--- ETS/MainPage.xaml.cs
+++ ETS/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace ETS;
+﻿using ETS.Models;
+using Javax.Security.Auth;
+using static Android.Provider.Contacts;
+
+namespace ETS;
 
 public partial class MainPage : ContentPage
 {
@@ -7,11 +11,19 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+       
+    }
 
 	private async void OnButtonClicked(object sender, EventArgs e)
 	{
 		await Navigation.PushAsync(new AddContact());
 	}
+
+	private async void GetPeople (object sender, EventArgs e)
+	{
+        List<CompanyContact> contacts = await App.CompanyContactRepo.GetAllPeople();
+        contactList.ItemsSource = contacts;
+    }
 }
 
