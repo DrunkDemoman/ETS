910bd4d5d2ce3e552da33fa94c8f7c2f6e799d4a
Ferdinand Putra Gumilang Silalahi <gumilangsilalahi3@gmail.con>
2023-03-27 07:53:59 +0700

    added database

---
+++ ETS/Models/Contacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+namespace ETS.Models
+{
+    [Table("contacts")]
+    public class Contacts
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100), Unique]
+        public string Name { get; set; }
+
+        [MaxLength (100)]
+        public string Company { get; set; }
+
+        [MaxLength (100)]
+        public string Email { get; set; }
+
+        [MaxLength (15)]
+        public string OfficeNumber { get; set; }
+
+        [MaxLength (15)]
+        public string PhoneNumber { get; set; }
+
+    }
+}
